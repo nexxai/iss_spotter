73528a8b3fa270bef8ae1ebe3929fa41ea26ae57
JT Smith <nexxai@gmail.com>
2023-08-31 14:36:56 -0600

    Implemented fetchCoordsByIP function

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned IP:", ip);
+
+  fetchCoordsByIP(ip, (error, coordinates) => {});
+});
---
+++ iss.js
@@ -0,0 +1,55 @@
+const req = require("request");
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+
+const fetchMyIP = function(callback) {
+  req("https://api.ipify.org?format=json", (error, response, body) => {
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      return callback(error, null);
+    }
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // if we get here, all's well and we got the data
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+const fetchCoordsByIP = function(ip, callback) {
+  req(`https://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const json = JSON.parse(body);
+
+    // if non-200 status, assume server error
+    if (!json.success) {
+      const msg = `Status Code: ${json.success} when fetching lat/long. Response: ${json.message}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // if we get here, all's well and we got the data
+    const latitude = json.latitude;
+    const longitude = json.longitude;
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
